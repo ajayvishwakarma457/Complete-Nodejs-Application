f04a431e81443dce9dce70f33b4b281544ab5fdf
Ajay <ajayvishwakarma457@gmail.com>
2025-05-19 08:24:10 +0530

    changes

--- src/workers/sayHello.job.js
+++ src/workers/sayHello.job.js
@@ -3,10 +3,4 @@ module.exports = function (agenda) {
   agenda.define('say hello', async (job) => {
     console.log('👋 Hello from Agenda at', new Date());
   });
-
-  (async function () {
-    await agenda.start();
-    await agenda.every("5 seconds", "say hello");
-  })();
-
 };
